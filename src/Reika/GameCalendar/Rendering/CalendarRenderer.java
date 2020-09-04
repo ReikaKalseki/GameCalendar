0920a8c671022f3ef4dd5826583db0f1feb0992d
Reika <reikasminecraft@gmail.com>
2020-09-04 16:08:14 -0400

    Update 16

--- src/Reika/GameCalendar/Rendering/CalendarRenderer.java
+++ src/Reika/GameCalendar/Rendering/CalendarRenderer.java
@@ -217,12 +217,12 @@ public class CalendarRenderer {
 			GL11.glPopMatrix();
 		}
 
+		int uniEnd = 2015;//years.get(years.size()-1);
 		if (JFXWindow.getGUI().getCheckbox("xmasBreak")) {
-			this.drawSplitTimeWedge(t, DateStamp.xmasStart, DateStamp.xmasFull, DateStamp.xmasEnd, 0xff0000, 0x007700);
+			this.drawSplitTimeWedge(t, DateStamp.xmasStart, DateStamp.xmasFull, DateStamp.xmasEnd, 0xff0000, 0x007700, uniEnd);
 		}
-
 		if (JFXWindow.getGUI().getCheckbox("summerBreak")) {
-			this.drawSplitTimeWedge(t, DateStamp.summerStart, DateStamp.summerFull, DateStamp.summerEnd, 0xffff00, 0x00ffff);
+			this.drawSplitTimeWedge(t, DateStamp.summerStart, DateStamp.summerFull, DateStamp.summerEnd, 0xffff00, 0x00ffff, uniEnd);
 		}
 
 		Labelling.instance.setRenderParams(sw, sh, this);
@@ -241,23 +241,32 @@ public class CalendarRenderer {
 		GL32.glDisableClientState(GL32.GL_VERTEX_ARRAY);*/
 	}
 
-	private void drawSplitTimeWedge(double t, DateStamp d1, DateStamp d2, DateStamp d3, int c1, int c2) {
+	private void drawSplitTimeWedge(double t, DateStamp d1, DateStamp d2, DateStamp d3, int c1, int c2, int maxyear) {
 		GL11.glEnable(GL11.GL_BLEND);
 		BlendMode.DEFAULT.apply();
 		float cm = (float)(0.5+0.5*Math.sin(t/400D));
-		GL11.glColor4f(1-cm, cm*0.5F, 0, 1);
+		int mix = Colors.mixColors(c1, c2, cm);
+		float red = Colors.getRed(mix)/255F;
+		float green = Colors.getGreen(mix)/255F;
+		float blue = Colors.getBlue(mix)/255F;
+		GL11.glColor4f(red, green, blue, 1);
 		GL11.glBegin(GL11.GL_LINE_LOOP);
 		GL11.glVertex2d(0, 0);
 		double a0 = d1.getAngle();
 		double a1 = d2.getAngle();
-		double a2 = d3.getAngle()+360;
+		double a2 = d3.getAngle();
+		if (a1 < a0)
+			a1 += 360;
+		if (a2 < a1)
+			a2 += 360;
 		double divAng = this.getGuiAngle(a1);
-		double divR = this.getArcCenterlineRadiusAt(years.size()-1, a1)+arcThickness*(arcThicknessHalfFraction+0.125);
+		int idx = years.indexOf(maxyear);
+		double divR = this.getArcCenterlineRadiusAt(idx, a1)+arcThickness*(arcThicknessHalfFraction+0.125);
 		double divX = divR*Math.cos(divAng);
 		double divY = divR*Math.sin(divAng);
 		for (double a = a0; a < a2; a += 0.5) {
 			double ang = this.getGuiAngle(a);
-			double r = this.getArcCenterlineRadiusAt(years.size()-1, a)+arcThickness*(arcThicknessHalfFraction+0.125);//MAX_THICKNESS;
+			double r = this.getArcCenterlineRadiusAt(idx, a)+arcThickness*(arcThicknessHalfFraction+0.125);//MAX_THICKNESS;
 			if (a >= 361)
 				r -= arcThickness;
 			double x = r*Math.cos(ang);
@@ -265,7 +274,7 @@ public class CalendarRenderer {
 			GL11.glVertex2d(x, y);
 		}
 		GL11.glEnd();
-		GL11.glColor4f(1-cm, cm*0.5F, 0, 0.5F);
+		GL11.glColor4f(red, green, blue, 0.5F);
 		GL11.glEnable(GL11.GL_LINE_STIPPLE);
 		GL11.glLineStipple(2, (short)0x7777);
 		GL11.glBegin(GL11.GL_LINES);
@@ -274,12 +283,12 @@ public class CalendarRenderer {
 		GL11.glEnd();
 		GL11.glDisable(GL11.GL_LINE_STIPPLE);
 		double f = 0.1875+0.0625*Math.sin(t/500D);
-		GL11.glColor4f(1-cm, cm*0.5F, 0, (float)f*0.4F);
+		GL11.glColor4f(red, green, blue, (float)f*0.4F);
 		GL11.glBegin(GL11.GL_TRIANGLE_FAN);
 		GL11.glVertex2d(0, 0);
 		for (double a = a0; a <= a1; a += 0.5) {
 			double ang = this.getGuiAngle(a);
-			double r = this.getArcCenterlineRadiusAt(years.size()-1, a)+arcThickness*(arcThicknessHalfFraction+0.125);//MAX_THICKNESS;
+			double r = this.getArcCenterlineRadiusAt(idx, a)+arcThickness*(arcThicknessHalfFraction+0.125);//MAX_THICKNESS;
 			if (a >= 361)
 				r -= arcThickness;
 			double x = r*Math.cos(ang);
@@ -288,12 +297,12 @@ public class CalendarRenderer {
 		}
 		GL11.glEnd();
 
-		GL11.glColor4f(1-cm, cm*0.5F, 0, (float)f);
+		GL11.glColor4f(red, green, blue, (float)f);
 		GL11.glBegin(GL11.GL_TRIANGLE_FAN);
 		GL11.glVertex2d(0, 0);
 		for (double a = a1; a <= a2; a += 0.5) {
 			double ang = this.getGuiAngle(a);
-			double r = this.getArcCenterlineRadiusAt(years.size()-1, a)+arcThickness*(arcThicknessHalfFraction+0.125);//MAX_THICKNESS;
+			double r = this.getArcCenterlineRadiusAt(idx, a)+arcThickness*(arcThicknessHalfFraction+0.125);//MAX_THICKNESS;
 			if (a >= 361)
 				r -= arcThickness;
 			double x = r*Math.cos(ang);
